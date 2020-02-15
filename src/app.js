40e2d94a536c2eebb4fde9199196a4b521ad3cb8
Thiago Vieira <th91v@outlook.com>
2020-02-15 16:40:31 -0300

    verifica quantidades de caracteres nos campos, igualdade nos campos de senhas, refatoracao da validacao do email

--- src/app.js
+++ src/app.js
@@ -14,9 +14,13 @@ const showError = (input, message) => {
 };
 
 // verifica se o e-mail eh valido
-const isValidEmail = email => {
+const checkEmail = input => {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid')
+  }
 };
 
 // mostra realce de sucesso
@@ -29,11 +33,29 @@ const showSuccess = input => {
 function checkRequired(inputArr){
   inputArr.forEach((input) => {
     if(input.value.trim() === ''){
-      showError(input, `${getFieldName(input)} é obrigatório`);
+      showError(input, `${getFieldName(input)} is required`);
     } else {
       showSuccess(input);
     }
   });
+}
+
+// verifica quantidade de caracteres nos campos
+function checkLength(input, min, max){
+  if(input.value.length < min){
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+  } else {
+    showSuccess(input);
+  }
+}
+
+// verifica que os valores de ambos campos de senhas sao iguais
+function checjPasswordsMatch(input1, input2){
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match')
+  }
 }
 
 // pega nome do campo pela id
@@ -42,9 +64,13 @@ function getFieldName(input){
   // recorta a primeira letra e a deixa maiuscula; depois junta com o resta da id, a partir da segunda letra
 }
 
-// event listeners
+// event listeners para validacao dos campos do formulario
 form.addEventListener("submit", e => {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3 , 15);
+  checkLength(password, 6 , 25);
+  checkEmail(email); // verifica validade do email, usando a variavel de email definida no inicio deste documento
+  checjPasswordsMatch(password, password2)
 });
